34c5374b31e74ca1bb8957e29d98e4ee0e6b01ac
Bradley <97bzhu@gmail.com>
2018-03-31 15:00:35 -0700

    notice me brian

--- Client/app/src/main/java/com/company/lahacks/lahacks2018/HomeActivity.java
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/HomeActivity.java
@@ -1,10 +1,23 @@
 package com.company.lahacks.lahacks2018;
 
+import android.view.View;
+
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public void updateImageUI() {
+        Intent intent = new Intent(this, ImageActivity.class);
+        startActivity(intent);
+    }
+
+    public void updateUploadUI() {
+        Intent intent = new Intent(this, UploadActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,17 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         Intent intent = getIntent();
+    }
+
+    public void createParty(View view) {
+        updateImageUI();
+    }
+
+    public void joinParty(View view) {
+        updateImageUI();
+    }
+
+    public void uploadImage(View view) {
+        updateUploadUI();
     }
 }
