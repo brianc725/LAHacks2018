391b1fcd785cd9ea3196b1b459fbd9abe78c50a7
Jeffrey Zhu <jeffreyzhu@Jeffreys-MacBook-Pro-3.local>
2018-03-31 12:42:26 -0700

    storing my change's - jeffrey

--- Client/app/src/main/java/com/company/lahacks/lahacks2018/MainActivity.java
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/MainActivity.java
@@ -14,12 +14,25 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private String email;
     private String password;
+    private static final String TAG = "MainActivity";
+
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("partyid");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        myRef.setValue("Hello, World, this is Jeff!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
 
     }
 
@@ -115,4 +148,15 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+
+
+
+
+
+
+
+
+
 }
+
