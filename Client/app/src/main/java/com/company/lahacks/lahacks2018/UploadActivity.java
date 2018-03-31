ebd3aeaba875c1eedb2962aa1a3872be2eb57c35
brianc725 <brianc725@gmail.com>
2018-03-31 15:25:04 -0700

    Finish uploading of photos with status updates

--- Client/app/src/main/java/com/company/lahacks/lahacks2018/UploadActivity.java
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/UploadActivity.java
@@ -3,6 +3,7 @@ package com.company.lahacks.lahacks2018;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 public class UploadActivity extends AppCompatActivity {
@@ -55,14 +60,36 @@ public class UploadActivity extends AppCompatActivity {
         if (imageUri == null) {
             Toast.makeText(UploadActivity.this, "Cannot upload without selecting image.",
                     Toast.LENGTH_SHORT).show();
-            return;
+            return; //no image so return immediately
         }
+
+        Toast.makeText(UploadActivity.this, "Please wait for upload...",
+                Toast.LENGTH_SHORT).show();
 
         //stores as 'photos/<filename>.jpg'
         final StorageReference photoRef = mStorageRef.child("photos")
                 .child(imageUri.getLastPathSegment());
 
-        photoRef.putFile(imageUri);
+        photoRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // Upload succeeded
+
+                // Get the public download URL
+                //Uri downloadUri = taskSnapshot.getMetadata().getDownloadUrl();
+
+                Toast.makeText(UploadActivity.this, "Photo successfully uploaded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(UploadActivity.this, "Photo upload failed. Please try again.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        })
+        ;
 
         //after uploading the image set imageURI back to null and clear imageview
         imageUri = null;
