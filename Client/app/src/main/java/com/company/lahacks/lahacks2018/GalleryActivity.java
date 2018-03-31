3936d63e658f9e5fb8be878fa0671193263fb6a9
Bradley <97bzhu@gmail.com>
2018-03-31 15:47:42 -0700

    :^)

---
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/GalleryActivity.java
@@ -0,0 +1,98 @@
+package com.company.lahacks.lahacks2018;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class GalleryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_images);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        GalleryActivity.ImageGalleryAdapter adapter = new GalleryActivity.ImageGalleryAdapter(this, MyPhoto.getPhotos());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>  {
+
+        @Override
+        public ImageGalleryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the layout
+            View photoView = inflater.inflate(R.layout.item_photo, parent, false);
+
+            ImageGalleryAdapter.MyViewHolder viewHolder = new ImageGalleryAdapter.MyViewHolder(photoView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ImageGalleryAdapter.MyViewHolder holder, int position) {
+
+            MyPhoto mPhoto = mPhotos[position];
+            ImageView imageView = holder.mPhotoImageView;
+
+            Glide.with(mContext)
+                    .load(MyPhoto.getUrl())
+//                    .placeholder(R.drawable.ic_cloud_off_red)
+                    .into(imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return (mPhotos.length);
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            public ImageView mPhotoImageView;
+
+            public MyViewHolder(View itemView) {
+
+                super(itemView);
+                mPhotoImageView = (ImageView) itemView.findViewById(R.id.iv_photo);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION) {
+                    MyPhoto mPhoto = mPhotos[position];
+
+                    //later if time create PhotoActivity
+//                    Intent intent = new Intent(mContext, PhotoActivity.class);
+//                    intent.putExtra(PhotoActivity.EXTRA_SPACE_PHOTO, mPhoto);
+//                    startActivity(intent);
+                }
+            }
+        }
+
+        private MyPhoto[] mPhotos;
+        private Context mContext;
+
+        public ImageGalleryAdapter(Context context, MyPhoto[] photos) {
+            mContext = context;
+            mPhotos = photos;
+        }
+    }
+}
---
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/MyPhoto.java
@@ -0,0 +1,71 @@
+package com.company.lahacks.lahacks2018;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MyPhoto implements Parcelable {
+
+    private static String mUrl;
+    private String mTitle;
+
+    public MyPhoto(String url, String title) {
+        mUrl = url;
+        mTitle = title;
+    }
+
+    protected MyPhoto(Parcel in) {
+        mUrl = in.readString();
+        mTitle = in.readString();
+    }
+
+    public static final Creator<MyPhoto> CREATOR = new Creator<MyPhoto>() {
+        @Override
+        public MyPhoto createFromParcel(Parcel in) {
+            return new MyPhoto(in);
+        }
+
+        @Override
+        public MyPhoto[] newArray(int size) {
+            return new MyPhoto[size];
+        }
+    };
+
+    public static String getUrl() {
+        return mUrl;
+    }
+
+    public void setUrl(String url) {
+        mUrl = url;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public static  MyPhoto[] getPhotos() {
+
+        return new MyPhoto[]{
+                new MyPhoto("http://i.imgur.com/zuG2bGQ.jpg", "Galaxy"),
+                new MyPhoto("http://i.imgur.com/ovr0NAF.jpg", "Space Shuttle"),
+                new MyPhoto("http://i.imgur.com/n6RfJX2.jpg", "Galaxy Orion"),
+                new MyPhoto("http://i.imgur.com/qpr5LR2.jpg", "Earth"),
+                new MyPhoto("http://i.imgur.com/pSHXfu5.jpg", "Astronaut"),
+                new MyPhoto("http://i.imgur.com/3wQcZeY.jpg", "Satellite"),
+        };
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mUrl);
+        parcel.writeString(mTitle);
+    }
+}
