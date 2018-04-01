a52ec182e4df9728fa8f494cd358c0440adaff42
Bradley <97bzhu@gmail.com>
2018-03-31 22:13:50 -0700

    here

--- Client/app/src/main/java/com/company/lahacks/lahacks2018/HostActivity.java
+++ Client/app/src/main/java/com/company/lahacks/lahacks2018/HostActivity.java
@@ -1,12 +1,14 @@
 package com.company.lahacks.lahacks2018;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -16,6 +18,9 @@ public class HostActivity extends AppCompatActivity {
 
     private boolean mLocationPermissionGranted = false;
     private FusedLocationProviderClient mFusedLocationClient;
+
+    private double lat;
+    private double lon;
 
 
     @SuppressLint("MissingPermission")
@@ -31,11 +36,17 @@ public class HostActivity extends AppCompatActivity {
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-                            double lat = location.getLatitude();
-                            double lon = location.getLongitude();
+                            lat = location.getLatitude();
+                            lon = location.getLongitude();
                         }
                     }
                 });
+    }
+
+    public void closeParty(View view) {
+        //close the party and push the lon, lat, and options
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 
 }
